e6161fc2c4554f626e3ac977c129c719842381b5
Sansa <pausansa59@gmail.com>
2023-03-11 11:30:05 +0100

    Add FileChooser

---
+++ src/DeveloperTeam/Application/WindowManager.java
@@ -0,0 +1,22 @@
+package DeveloperTeam.Application;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+public class WindowManager {
+    public static File getDir(){
+        String path = "";
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.setDialogTitle("Selecciona la carpeta de guardado del Ticket");
+
+        int result = fileChooser.showOpenDialog(null);
+        if(result==JFileChooser.APPROVE_OPTION){
+            path = fileChooser.getSelectedFile().getAbsolutePath();
+        } else{
+            System.out.println("Se ha cancelado el proceso");
+        }
+        return new File(path + "/ticket.txt");
+    }
+}
--- src/DeveloperTeam/MainTestingPau.java
+++ src/DeveloperTeam/MainTestingPau.java
@@ -1,9 +1,11 @@
 package DeveloperTeam;
 
 import DeveloperTeam.Application.App;
+import DeveloperTeam.Application.WindowManager;
 
 public class MainTestingPau {
     public static void main(String[] args) {
+        WindowManager.getDir();
         App app = new App();
         App app2 = new App();
         App app3 = new App();
