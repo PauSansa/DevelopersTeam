3af1fc85fe9028dc380d131ead2db39148bac226
Sansa <pausansa59@gmail.com>
2023-03-19 19:15:39 +0100

    Updating

--- src/main/java/DeveloperTeam/Model/Repository/Repository.java
+++ src/main/java/DeveloperTeam/Model/Repository/Repository.java
@@ -7,13 +7,17 @@ import java.io.IOException;
 import java.util.List;
 
 public interface Repository {
+    //Stock
     void addStockItem(IArticle art) throws Exception;
     List<IArticle> getAll() throws Exception;
     void removeStockItem(int idArticle) throws Exception;
     boolean exists(int idArticle) throws Exception;
     IArticle getOne(int idArticle) throws Exception;
+    int countStock() throws IOException;
+
+    //Ticket
     List<Ticket> getAllTickets() throws Exception;
     void insertTicket(Ticket ticket) throws Exception;
-    int countStock() throws IOException;
+
 
 }
